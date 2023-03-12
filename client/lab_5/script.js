f71744a1bfbdef8bbb18a8a33b6c8920aae7a4bc
Evan Montgomery <emont17@terpmail.umd.edu>
2023-03-12 18:11:02 -0400

    added to file

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -3,60 +3,53 @@
   by adding `<script src="script.js">` just before your closing `</body>` tag
 */
 
+/* A quick filter that will return something based on a matching input */
+function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  })
+  /* 
+    Using the .filter array method,
+    return a list that is filtered by comparing the item name in lower case
+    to the query in lower case
+
+    Ask the TAs if you need help with this
+  */
+
+}
+
 async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+  const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const filterButton = document.querySelector('.filter_button');
+
+  let currentList = [];
+
+
+
+  mainForm.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
     submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
     console.log('form submission'); // this is substituting for a "breakpoint"
 
-    /*
-      ## GET requests and Javascript
-        We would like to send our GET request so we can control what we do with the results
-        But this blocks us sending a query string by default - ?resto='' won't exist
-
-        Let's get those form results before sending off our GET request using the Fetch API
-    */
-
-    // this is the preferred way to handle form data in JS in 2022
-    const formData = new FormData(submitEvent.target); // get the data from the listener target
-    const formProps = Object.fromEntries(formData); // Turn it into an object
-
-    // You can also access all forms in a document by using the document.forms collection
-    // But this will retrieve ALL forms, not just the one that "heard" a submit event - less good
-
-    /*
-      ## Retrieving information from an API
-        The Fetch API is relatively new,
-        and is much more convenient than previous data handling methods.
-        Here we make a basic GET request to the server using the Fetch method
-        to send a request to the routes defined in /server/routes/foodServiceRoutes.js
-
-      // this is a basic GET request
-      // It does not include any of your form values, though
-    */
-
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-    /*
-   ## Get request with query parameters
 
-      const results = await fetch(`/api/foodServicePG?${new URLSearchParams(formProps)}`);
+    currentList = await results.json();
 
-      The above request uses "string interpolation" to include an encoded version of your form values
-      It works because it has a ? in the string
-      Replace line 37 with it, and try it with a / instead to see what your server console says
+    console.table(currentList);
+});
 
-      You can check what you sent to your server in your GET request
-      By opening the "network" tab in your browser developer tools and looking at the "name" column
-      This will also show you how long it takes a request to resolve
-    */
+filterButton.addEventListener('click', (event) => {
+  console.log('clicked filterButton');
 
-    // This changes the response from the GET into data we can use - an "object"
-    const arrayFromJson = await results.json();
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it initially contains all 1,000 records from your request
-  });
-}
+  const formData = new FormData(mainForm);
+  const formProps = Object.fromEntries(formData);
+
+  console.log(formProps);
+  const newList = filterList(currentList, formProps.resto);
+
+  console.log(newList);
+})
 
 /*
   This adds an event listener that fires our main event only once our page elements have loaded
